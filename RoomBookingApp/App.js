3e09d3a643dade927eb47d829676e03cbe1cabcf
caseyvtcd <72365399+caseyvtcd@users.noreply.github.com>
2022-02-17 19:46:11 +0000

    Adding Updated App.js File to the repository
    Includes all Questions

--- RoomBookingApp/App.js
+++ RoomBookingApp/App.js
@@ -13,15 +13,29 @@ class App extends React.Component {
     super();
     this.state = {
       isVisible: false,
-      nameOfEvent: "",
-      dateOfEvent: "",
-      organisingBody: "",
-      organiserName: "",
-      mobileNumber: "",
-      tcdEmail: ""+"@tcd.ie",
-      eventDescription: "",
-      building: "",
-      room: ""
+      nameOfEvent:"",
+      dateOfEvent:"",
+      organisingBody:"",
+      organiserName:"",
+      mobileNumber:"",
+      tcdEmail:"",
+      eventDescription:"",
+      room:"",
+      prepTo:"",
+      prepFrom:"",
+      startTime:"",
+      endTime:"",
+      numParticipants:"",
+      numStaff:"",
+      guests:"",
+      equipment:"",
+      staging:"",
+      food:"",
+      alcohol:"",
+      caterer:"",
+      power:"",
+      facilities:"",
+      other:"",
     }
   }
 
@@ -79,38 +93,145 @@ class App extends React.Component {
 
           <TextInput
             placeholder='Organising Body'
-            onChangeText={(text) => { this.setState({ organisingBody: text }) }}
-            style={styles.orgBody}
-          />
-
-          <TextInput
-            placeholder='Organiser Name'
-            onChangeText={(text) => { this.setState({ organiserName: text }) }}
-            style={styles.orgName}
+            onChangeText={(text) => { this.setState({ organisingBody: text}) }}
+            style={{ borderWidth: 1, borderColor: 'gray', padding: 10, marginTop: 20 }}
           />
 
           <TextInput
             placeholder='Mobile Number'
-            keyboardType='numeric'
-            onChangeText={(number) => { this.setState({ mobileNumber: number }) }}
-            style={styles.mobileNum}
+            onChangeText={(number) => { this.setState({ mobileNumber: number}) }}
+            style={{ borderWidth: 1, borderColor: 'gray', padding: 10, marginTop: 20 }}
           />
 
           <TextInput
-            placeholder='TCD Username'
-            keyboardType='email-address'
-            onChangeText={(text) => { this.setState({ tcdEmail: text + "@tcd.ie"}) }}
-            style={styles.tcdEmail}
+            placeholder='TCD Email'
+            onChangeText={(text) => { this.setState({ tcdEmail: text}) }}
+            style={{ borderWidth: 1, borderColor: 'gray', padding: 10, marginTop: 20 }}
           />
 
           <TextInput
-            multiline={true}
+            multiline
+            numberOfLines={5}
             placeholder='Event Description/Outline'
-            onChangeText={(text) => { this.setState({ eventDescription: text }) }}
-            style={styles.eventDesc}
+            onChangeText={(text) => { this.setState({ eventDescription: text}) }}
+            style={{ borderWidth: 1, borderColor: 'gray', padding: 10, marginTop: 20 }}
           />
+        
 
           <Text style={styles.heading2}>Where is your event?</Text>
+
+          <TextInput
+            placeholder='Room of Event'
+            onChangeText={(text) => { this.setState({ room: text}) }}
+            style={{ borderWidth: 1, borderColor: 'grey', padding: 10, marginTop: 20 }}
+          />
+        
+        <TextInput
+            placeholder='Preparation Time From:'
+            onChangeText={(text) => { this.setState({ prepFrom: text}) }}
+            style={{ borderWidth: 1, borderColor: 'grey', padding: 10, marginTop: 20 }}
+          />
+
+        <TextInput
+            placeholder='Preparation Time To:'
+            onChangeText={(text) => { this.setState({ prepTo: text}) }}
+            style={{ borderWidth: 1, borderColor: 'grey', padding: 10, marginTop: 20 }}
+          />
+
+        <TextInput
+            placeholder='Event Time From:'
+            onChangeText={(text) => { this.setState({ startTime: text}) }}
+            style={{ borderWidth: 1, borderColor: 'grey', padding: 10, marginTop: 20 }}
+          />
+
+        <TextInput
+            placeholder='Event Time To:'
+            onChangeText={(text) => { this.setState({ endTime: text}) }}
+            style={{ borderWidth: 1, borderColor: 'grey', padding: 10, marginTop: 20 }}
+          />
+
+
+
+
+        <Text style={{ fontSize: 20, textAlign: 'center' }}>Event Details</Text>
+
+        <TextInput
+            placeholder='Number of Participants'
+            onChangeText={(text) => { this.setState({ numParticipants: text}) }}
+            style={{ borderWidth: 1, borderColor: 'grey', padding: 10, marginTop: 20 }}
+          />
+
+        <TextInput
+            placeholder='Number of Event Staff'
+            onChangeText={(text) => { this.setState({ numStaff: text}) }}
+            style={{ borderWidth: 1, borderColor: 'grey', padding: 10, marginTop: 20 }}
+          />  
+
+          <TextInput
+            placeholder='Special Guests or VIPs'
+            onChangeText={(text) => { this.setState({ guests: text}) }}
+            style={{ borderWidth: 1, borderColor: 'grey', padding: 10, marginTop: 20 }}
+          />  
+
+          <TextInput
+            multiline
+            numberOfLines={5}
+            placeholder='Equipment'
+            onChangeText={(text) => { this.setState({ equipment: text}) }}
+            style={{ borderWidth: 1, borderColor: 'gray', padding: 10, marginTop: 20 }}
+          />
+
+          <TextInput
+            multiline
+            numberOfLines={5}
+            placeholder='Staging/Room Setup'
+            onChangeText={(text) => { this.setState({ staging: text}) }}
+            style={{ borderWidth: 1, borderColor: 'gray', padding: 10, marginTop: 20 }}
+          />
+
+          <TextInput
+            placeholder='Food Served?'
+            onChangeText={(text) => { this.setState({ food: text}) }}
+            style={{ borderWidth: 1, borderColor: 'grey', padding: 10, marginTop: 20 }}
+          />  
+
+          <TextInput
+            placeholder='Alcohol Served?'
+            onChangeText={(text) => { this.setState({ alcohol: text}) }}
+            style={{ borderWidth: 1, borderColor: 'grey', padding: 10, marginTop: 20 }}
+          />  
+
+          <TextInput
+            placeholder='Using Caterer?'
+            onChangeText={(text) => { this.setState({ caterer: text}) }}
+            style={{ borderWidth: 1, borderColor: 'grey', padding: 10, marginTop: 20 }}
+          />  
+
+        <Text style={{  margin: 20, top: 10, fontSize: 20, textAlign: 'center' }}>Additional Info</Text>
+
+        <TextInput
+            placeholder='Power Source Required?'
+            onChangeText={(text) => { this.setState({ power: text}) }}
+            style={{ borderWidth: 1, borderColor: 'grey', padding: 10, marginTop: 20 }}
+          />  
+
+          
+        <TextInput
+            multiline
+            numberOfLines={5}
+            placeholder='Facilities for disabled persons'
+            onChangeText={(text) => { this.setState({ facilities: text}) }}
+            style={{ borderWidth: 1, borderColor: 'gray', padding: 10, marginTop: 20 }}
+          />
+
+          
+        <TextInput
+            multiline
+            numberOfLines={5}
+            placeholder='Other Requirements'
+            onChangeText={(text) => { this.setState({ other: text}) }}
+            style={{ borderWidth: 1, borderColor: 'gray', padding: 10, marginTop: 20 }}
+          />
 
           {/* <Button
             title='Submit Request' onPress={() => { this.submit() }}
