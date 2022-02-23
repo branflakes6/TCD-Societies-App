1377a5ecf5f56421fc04ee4f8276531b9378fac9
Conor <oneilc51@tcd.ie>
2022-02-23 13:19:18 -0500

    updates to home screen

--- RoomBookingApp/components/Homescreen.js
+++ RoomBookingApp/components/Homescreen.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import styles from '../styles/styles';
+import logo from './images/Logo.png';
 
 const Home = ({ navigation }) => {
     return (
         <View style={styles.container}>
             <Text style={styles.headerText}>
-                Welcome to Homescreen!
+                Welcome to the Trinity Room Book!
             </Text>
+            <img src={logo} alt="logo" />
+
             <TouchableOpacity
                 style={styles.button}
-                onPress={() => navigation.navigate("About")}>
+                onPress={() => navigation.navigate("Form")}>
                 <Text style={styles.buttonText}>
-                    Go to Aboutscreen
+                    Request a Room
                 </Text>
 
             </TouchableOpacity>
             <TouchableOpacity
                 style={styles.button}
-                onPress={() => navigation.navigate("Form")}>
+                onPress={() => navigation.navigate("About")}>
                 <Text style={styles.buttonText}>
-                    Go to Formscreen
+                    View Bookings
                 </Text>
 
             </TouchableOpacity>
