bb37ddab5189401a80069e189b1cbcd51ead6783
Suman Neupane <sumanneupane99@gmail.com>
2022-02-28 20:50:25 +0000

    implemented datetime picker

--- RoomBookingApp/components/Formscreen.js
+++ RoomBookingApp/components/Formscreen.js
@@ -1,16 +1,42 @@
+
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
+import { View, Button, Platform, Text } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { State, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import moment from 'moment';
 import styles from '../styles/styles';
 
-const Form = ({ navigation }) => {
+const Form = ({navigation }) => {
     const [name, nameOfEvent] = React.useState('');
-    const [date, dateOfEvent] = React.useState('');
+    const [date, dateOfEvent] = React.useState(new Date());
     const [time, timeOfEvent] = React.useState('');
     const [orginiser, organisingBody] = React.useState('');
     const [number, mobileNumber] = React.useState('');
     const [emails, email] = React.useState('');
 
+    const [mode, setMode] = React.useState('');
+    const [show, setShow] = React.useState(false);
+    
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate;
+        setShow(Platform.OS === 'ios');
+        dateOfEvent(currentDate);
+        timeOfEvent(moment(currentDate).format('HH:mm'));
+    };
+
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
+
+    const showTimepicker = () => {
+        showMode('time');
+    };
 
     return (
         <View style={styles.container}>
@@ -26,12 +52,19 @@ const Form = ({ navigation }) => {
                 </Text>
 
             </TouchableOpacity>
+
             <TextInput
                 style={styles.input}
                 placeholder="Name of Event"
-                onChangeText={nameOfEvent}
+                onChangeText={(text) => nameOfEvent(text)}
                 value={name}
             />
+            <View style={styles.datetimeButton}>
+                <Button title="Date of Event" onPress={showDatepicker} color='#f4511e'/>
+                <Button title="Time of Event" onPress={showTimepicker} onChange={timeOfEvent} color='#f4511e'/> 
+            </View>
+            
+
             <TextInput
                 style={styles.input}
                 placeholder="Email"
@@ -44,18 +77,7 @@ const Form = ({ navigation }) => {
                 onChangeText={mobileNumber}
                 value={number}
             />
-            <TextInput
-                style={styles.input}
-                placeholder="Date of Event"
-                onChangeText={dateOfEvent}
-                value={date}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Time of Event"
-                onChangeText={timeOfEvent}
-                value={time}
-            />
+
             <TextInput
                 style={styles.input}
                 placeholder="Organising Body"
@@ -77,6 +99,15 @@ const Form = ({ navigation }) => {
                     Request
                 </Text>
             </TouchableOpacity>
+            {show && (
+                <DateTimePicker
+                    value={date}
+                    mode={mode}
+                    is24Hour={true}
+                    display="default"
+                    onChange={onChange}
+                />
+            )}
         </View>
     );
 };
