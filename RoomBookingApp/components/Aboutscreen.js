b46bba9e0cc464a4d9bef2a5525a1363973bbffe
Brian Farrell <farrelb6@tcd.ie>
2022-03-03 19:20:51 +0000

    booking page

--- RoomBookingApp/components/Aboutscreen.js
+++ RoomBookingApp/components/Aboutscreen.js
@@ -1,23 +1,54 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import styles from '../styles/styles';
+import React, { useState,  } from 'react'
+import { FlatList, Text, TextInput, TouchableOpacity, View, Button, ScrollView  } from 'react-native'
+import styles from '../src/tst/styles.js';
+
+import { readBooking } from '../src/firebase/read'
+import { BookingTile } from '../src/components/bookingTile'
+
+
 
 const About = ({ navigation }) => {
+    const [emailRead, setEmailRead] = useState('')
+    const [entities, setEntities] = useState([])
+ 
+    function callRead() {
+        var propData = {
+            setEntities:setEntities,
+            email:"dog@tcd.ie",
+        }
+        readBooking(propData)
+    }
+    const renderEntity = ({item, index}) => {
+        return (
+        <BookingTile props={item}/> 
+        )
+
+    }
+
     return (
+        <ScrollView>
         <View style={styles.container}>
-            <Text style={styles.headerText}>
-                Welcome to Your Bookings!
-            </Text>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => navigation.navigate("Home")}>
-                <Text style={styles.buttonText}>
-                    Go to Homescreen
-                </Text>
+        <View style={styles.formContainer}>
+            <Text style={styles.heading1}>Read Bookings from DB</Text>
+            <TouchableOpacity style={styles.button} onPress={() => callRead()} >
+                <Text style={styles.buttonText}>Read Bookings</Text>
             </TouchableOpacity>
+
+
+            { entities && (
+                <View style={styles.listContainer}>
+                    <FlatList
+                        data={entities}
+                        renderItem={renderEntity}
+                        keyExtractor={(item) => item.id}
+                        removeClippedSubviews={true}
+                    />
+                </View>
+            )}
         </View>
-    );
+    </View>
+    </ScrollView>
+    )
 };
 
 export default About;
