fc202a9de891a115383e05992e8f43d53b1d2c01
Brian Farrell <farrelb6@tcd.ie>
2022-03-03 15:37:13 +0000

    React Native Booking Tile

--- RoomBookingApp/src/components/bookingTile.js
+++ RoomBookingApp/src/components/bookingTile.js
@@ -1,25 +1,34 @@
 import React, { useState } from 'react'
-import { FlatList, Text, TextInput, TouchableOpacity, View, Button  } from 'react-native'
+import { FlatList, Text, TextInput, TouchableOpacity, View} from 'react-native'
 import styles from '../tst/styles.js';
-
+//import { Provider as PaperProvider } from 'react-native-paper';
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper'
 import { readBooking } from '../firebase/read'
 
 export function BookingTile(props) {
     
     var str = ""
     const booking = props.props
+    console.log(booking)
     Object.keys(booking).forEach(function(key){
         str += key + ": " + booking[key] + "\n"
     }) 
 
 
-    return (
+    return ( 
         <View style={styles.container}>
         <View style={styles.formContainer}>
-        <Text style={styles.heading1}>This is a booking</Text>
-            <Text style={styles.entityText}>
-                {str}
-            </Text>
+            <Card>
+            <Card.Title title={booking.nameOfEvent} subtitle={booking.dateOfEvent}/>
+            <Card.Content>
+                <Title>Card title</Title>
+                <Paragraph>{booking.eventDescription}</Paragraph>
+            </Card.Content>
+            <Card.Actions>
+                <Button>Aprove</Button>
+                <Button>Deny</Button>
+            </Card.Actions>
+            </Card>
         </View>
     </View>
     )
