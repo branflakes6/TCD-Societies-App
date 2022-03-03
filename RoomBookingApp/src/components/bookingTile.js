9cce454451e6907704e17f81c4319e45b88e348a
Brian Farrell <farrelb6@tcd.ie>
2022-03-03 16:11:11 +0000

    Outlined tiles and confirmation boxes

--- RoomBookingApp/src/components/bookingTile.js
+++ RoomBookingApp/src/components/bookingTile.js
@@ -2,34 +2,58 @@ import React, { useState } from 'react'
 import { FlatList, Text, TextInput, TouchableOpacity, View} from 'react-native'
 import styles from '../tst/styles.js';
 //import { Provider as PaperProvider } from 'react-native-paper';
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper'
+import { Avatar, Button, Card, Title, Paragraph, Divider, Dialog, Portal, Provider } from 'react-native-paper'
 import { readBooking } from '../firebase/read'
+
 
 export function BookingTile(props) {
     
-    var str = ""
     const booking = props.props
-    console.log(booking)
-    Object.keys(booking).forEach(function(key){
-        str += key + ": " + booking[key] + "\n"
-    }) 
+    const LeftContent = props => <Avatar.Icon {...props} icon="calendar"/>
+    const [visibleAp, setVisibleAp] = React.useState(false);
+    const [visibleDen, setVisibleDen] = React.useState(false);
 
+    const confirmApprove = () => setVisibleAp(!visibleAp);
+    const confirmDeny = () => setVisibleDen(!visibleDen);
 
     return ( 
-        <View style={styles.container}>
-        <View style={styles.formContainer}>
-            <Card>
-            <Card.Title title={booking.nameOfEvent} subtitle={booking.dateOfEvent}/>
+        <Provider>
+        <View>
+            <Card mode="outlined">
+            <Card.Title title={booking.nameOfEvent} subtitle={booking.dateOfEvent} left={LeftContent}/>
             <Card.Content>
                 <Title>Card title</Title>
-                <Paragraph>{booking.eventDescription}</Paragraph>
+                <Paragraph>{booking.eventDescription} {booking.startTime}</Paragraph>
             </Card.Content>
             <Card.Actions>
-                <Button>Aprove</Button>
-                <Button>Deny</Button>
+                <Button mode="contained" color="#65db56" onPress={confirmApprove}>Aprove</Button>
+                <Portal>
+                <Dialog visible={visibleAp} onDismiss={confirmApprove}>
+                    <Dialog.Title>Alert</Dialog.Title>
+                    <Dialog.Content>
+                    <Paragraph>This is simple dialog</Paragraph>
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                    <Button onPress={confirmApprove}>Done</Button>
+                    </Dialog.Actions>
+                </Dialog>
+                </Portal>
+                <Button mode="contained" color="#c23838" onPress={confirmDeny}>Deny</Button>
+                <Portal>
+                <Dialog visible={visibleDen} onDismiss={confirmDeny}>
+                    <Dialog.Title>Alert</Dialog.Title>
+                    <Dialog.Content>
+                    <Paragraph>This is simple dialog</Paragraph>
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                    <Button onPress={confirmDeny}>Done</Button>
+                    </Dialog.Actions>
+                </Dialog>
+                </Portal>
             </Card.Actions>
-            </Card>
-        </View>
+            </Card> 
+            <Divider/>
     </View>
+    </Provider>
     )
 }
