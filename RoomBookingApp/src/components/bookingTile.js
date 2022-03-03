c706a30066dc0e3a8ec7f0a04de12a7a9095d966
Brian Farrell <farrelb6@tcd.ie>
2022-03-03 18:10:02 +0000

    New design and more info

--- RoomBookingApp/src/components/bookingTile.js
+++ RoomBookingApp/src/components/bookingTile.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react'
-import { FlatList, Text, TextInput, TouchableOpacity, View} from 'react-native'
-import styles from '../tst/styles.js';
-//import { Provider as PaperProvider } from 'react-native-paper';
-import { Avatar, Button, Card, Title, Paragraph, Divider, Dialog, Portal, Provider } from 'react-native-paper'
-import { readBooking } from '../firebase/read'
+import { View, StyleSheet} from 'react-native'
+import { List, Avatar, Button, Card, Title, Paragraph, Divider, Dialog, Portal, Provider, Subheading, Text } from 'react-native-paper'
+
 
 
 export function BookingTile(props) {
@@ -12,41 +10,96 @@ export function BookingTile(props) {
     const LeftContent = props => <Avatar.Icon {...props} icon="calendar"/>
     const [visibleAp, setVisibleAp] = React.useState(false);
     const [visibleDen, setVisibleDen] = React.useState(false);
+    const [description, viewDescription] = React.useState(false)
+
+    const [expanded, setExpanded] = React.useState(true);
 
     const confirmApprove = () => setVisibleAp(!visibleAp);
     const confirmDeny = () => setVisibleDen(!visibleDen);
+    const showDescription = () => viewDescription(!description)
 
+    const eventTitle = booking.nameOfEvent + " - " + booking.organisingBody
+    const persons = parseInt(booking.numParticipants) + parseInt(booking.numStaff) + parseInt(booking.guests)
+    const eventTime = booking.dateOfEvent + " - " + booking.timeOfEvent
+
+    const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+        },
+        card: {
+            paddingHorizontal: 0
+        }
+      });
+    
+    function approveBooking () {
+        setVisibleAp(!visibleAp)
+        console.log("Approved")
+    }
+    function denyBooking () {
+        setVisibleAp(!visibleDen)
+        console.log("Denied")
+    }
     return ( 
+        
         <Provider>
         <View>
-            <Card mode="outlined">
-            <Card.Title title={booking.nameOfEvent} subtitle={booking.dateOfEvent} left={LeftContent}/>
+            <Card mode="outlined" style={styles.card}>
+            <Card.Title title={eventTitle} subtitle={eventTime} left={LeftContent}/>
             <Card.Content>
-                <Title>Card title</Title>
-                <Paragraph>{booking.eventDescription} {booking.startTime}</Paragraph>
+                <Title> Current Booking Status: {booking.status}</Title>
+                <List.Section>
+                    <List.Accordion title="Booking Details">
+                        <List.Item title="Room" description={booking.room} />
+                        <List.Item title="Persons" description={persons}/>
+                        <List.Item title="Food" description={booking.food}/>
+                        <List.Item title="Alcohol" description={booking.alcohol}/>
+                        <List.Item title="Caterer" description={booking.caterer}/>
+                        <List.Item title="Description" description={booking.eventDescription} onPress={showDescription}/>
+                        <Portal>
+                        <Dialog visible={description} onDismiss={showDescription}>
+                            <Dialog.Title>Description</Dialog.Title>
+                            <Dialog.Content>
+                            <Paragraph>{booking.eventDescription}</Paragraph>
+                            </Dialog.Content>
+                            <Dialog.Actions>
+                            <Button onPress={showDescription}>Close</Button>
+                            </Dialog.Actions>
+                        </Dialog>
+                        </Portal>
+                    </List.Accordion>
+                </List.Section>
+                <List.Section>
+                    <List.Accordion title="Contact Details">
+                        <List.Item title="Organiser Name" description={booking.orginiserName}/>
+                        <List.Item title="Organiser Email" description={booking.email}/>
+                        <List.Item title="Organiser Phone Number" description={booking.mobileNumber}/>
+                    </List.Accordion>
+                </List.Section>
             </Card.Content>
             <Card.Actions>
                 <Button mode="contained" color="#65db56" onPress={confirmApprove}>Aprove</Button>
                 <Portal>
                 <Dialog visible={visibleAp} onDismiss={confirmApprove}>
-                    <Dialog.Title>Alert</Dialog.Title>
+                    <Dialog.Title>Conformation</Dialog.Title>
                     <Dialog.Content>
-                    <Paragraph>This is simple dialog</Paragraph>
+                    <Paragraph>Are you sure you want to confirm this booking?</Paragraph>
                     </Dialog.Content>
                     <Dialog.Actions>
-                    <Button onPress={confirmApprove}>Done</Button>
+                    <Button onPress={approveBooking}>Yes</Button>
+                    <Button onPress={confirmApprove}>No</Button>
                     </Dialog.Actions>
                 </Dialog>
                 </Portal>
                 <Button mode="contained" color="#c23838" onPress={confirmDeny}>Deny</Button>
                 <Portal>
                 <Dialog visible={visibleDen} onDismiss={confirmDeny}>
-                    <Dialog.Title>Alert</Dialog.Title>
+                    <Dialog.Title>Conformation</Dialog.Title>
                     <Dialog.Content>
-                    <Paragraph>This is simple dialog</Paragraph>
+                    <Paragraph>Are you sure you want to deny this booking?</Paragraph>
                     </Dialog.Content>
                     <Dialog.Actions>
-                    <Button onPress={confirmDeny}>Done</Button>
+                    <Button onPress={denyBooking}>Yes</Button>
+                    <Button onPress={confirmDeny}>No</Button>
                     </Dialog.Actions>
                 </Dialog>
                 </Portal>
